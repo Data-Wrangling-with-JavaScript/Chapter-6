180413cf373d781c40f4dc87ceb0a47da4dfd3fe
Ashley Davis <ashley@codecapers.com.au>
2017-10-11 09:18:29 +1000

    Removed debug logging.

--- listing-11.js
+++ listing-11.js
@@ -13,14 +13,6 @@ var importDateFormat = "YYYY-MM-DD HH:mm:ss";
 dataForge.readFile('./data/surveys.csv')
     .parseCSV()
     .then(dataFrame => {
-        console.log(
-dataFrame.subset(["start_datetime", "end_datetime", "dive_temperature"])
-            .parseDates(["start_datetime", "end_datetime"], importDateFormat)
-            .where(row => moment(row.start_datetime).year() === 2014)
-            .where(row => row.dive_temperature !== 0)            
-            .toString()
-        ); //fio:
-
         return dataFrame.dropSeries(["exp_id", "dive_observations", "obs_topography"])
             .parseDates(["start_datetime", "end_datetime"], importDateFormat)
             .where(row => moment(row.start_datetime).year() === 2014)
