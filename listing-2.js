ed99bfa4d7386b1d958ba19a6b743b4e48236f9c
Ashley Davis <ashley@codecapers.com.au>
2017-10-13 08:39:48 +1000

    New code listing.

---
+++ listing-2.js
@@ -0,0 +1,40 @@
+//
+// Example that imports a CSV file, fixes date/time values in each row and exporting a new CSV file.
+// Date/times are fixed by encoding in UTC so that the correct timezone is preserved.
+//
+
+'use strict';
+
+var moment = require('moment');
+var extend = require('extend');
+var importCsvFile = require('./toolkit/importCsvFile.js');
+var exportCsvFile = require('./toolkit/exportCsvFile.js');
+
+var importDateFormat = "YYYY-MM-DD HH:mm:ss";
+
+function fixRow (inputRow) {
+    var outputRow = extend({}, inputRow);
+    outputRow.start_datetime = moment(inputRow.start_datetime, importDateFormat).toDate();
+    outputRow.end_datetime = moment(inputRow.end_datetime, importDateFormat).toDate();
+    return outputRow;
+}
+
+function transformData (inputData) {
+    return inputData.map(fixRow);
+}
+
+importCsvFile('./data/surveys.csv')
+    .then(inputData => {
+        var outputData = transformData(inputData);
+        return exportCsvFile(
+            './output/surveys-with-fixed-dates.csv', 
+            outputData
+        );
+    })
+    .then(() => {
+        console.log('Done!');
+    })
+    .catch(err => {
+        console.error('Error!');
+        console.error(err && err.stack || err);
+    });
